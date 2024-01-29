246117f24320503017f4492372eaecc05af6af3d
VardanHeroic <tarayanvardan@gmail.com>
2024-01-29 19:30:00 +0400

    idk

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 var path = require("path");
 const bodyParser = require('body-parser');
 var app = express();
-const connectionString = 'mongodb+srv://VardanHeroic:kNukERA7pRwhwATT@cluster0.v2ffyjn.mongodb.net/sample_mflix';
+const connectionString = 'mongodb+srv://VardanHeroic:kNukERA7pRwhwATT@cluster0.v2ffyjn.mongodb.net/tumo';
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +12,45 @@ app.use(bodyParser.json())
 
 app.use(express.static('public'));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,'./public/form.html'));
+app.get("/", (req, res) => {
+	mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+	const db = mongoose.connection;
+	let productSchema = new mongoose.Schema({
+		title: String,
+		brand: String,
+		description: String,
+		thumbnail: String,
+		price: Number,
+	})
+	let Product = mongoose.model('Product', productSchema)
+	db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+		console.log('Connected to MongoDB!');
+		await mongoose.connection.db.collection("products").insertOne(new Product({title: 'sasha', brand: "jklsadf", description: "sadf", thumbnail: "jkdfs", price: 45809}).save())
+	})
+})
+
+app.get("/list", function(req, res){
+	let movies = []
+	mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+	const db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+		console.log('Connected to MongoDB!');
+		try {
+			movies = await mongoose.connection.db.collection('theaters').find( {"location.address.city": "Bloomington"}).toArray()
+			console.log(movies);
+			res.render('../public/form.ejs',{
+				nums: movies
+			})
+		} catch (error) {
+			console.error('Error retrieving movies:', error);
+		} finally {
+			mongoose.connection.close();
+		}
+	})
+
+
 });
 
 app.post('/addName', (req, res) => {
